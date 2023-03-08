31cce62affc341560930e7f9a4c3dc08922da121
Mauferva <98562540+Mauferva@users.noreply.github.com>
2023-03-08 16:51:49 -0300

    Add files via upload

---
+++ trabajo final/script.js
@@ -0,0 +1,6 @@
+var nombre = prompt ('ingrese su nombre')
+if (nombre == 'no') {
+    console.log ('igual lo mostraremos')
+}
+alert('hola ' + nombre + ' te mostraremos a nuetro mejor postulante')
+
